df612682667ecbe5b89206f7fef5cde23e609681
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-27 16:17:10 +0300

    task multiply by 2 completed

---
+++ multiply-numbers-by-2/multiply-number-by-2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Write a function that receives a number and prints as result that number multiplied by two.
+
+function multiplyByTwo(input) {
+  console.log(input * 2);
+}
+
+multiplyByTwo(5);
