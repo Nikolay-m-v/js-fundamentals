02604cfaaf1c350e39183519f4bca8c736a9810a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-27 17:00:55 +0300

    1 to 5 completed

---
+++ multiply-numbers-by-2/numbers-1-to-5.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
+
+function numbersOneToFive() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+
+numbersOneToFive();
