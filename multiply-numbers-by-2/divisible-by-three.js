98b06f8b499bcd6623d635547110462e08bb82df
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-27 17:03:12 +0300

    Divisible by 3 completed

---
+++ multiply-numbers-by-2/divisible-by-three.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Write a program, which prints all the numbers from 1 to 100, which are divisible by 3.
+// You have to use a single for loop. The program should not receive input.
+
+function divisibleByThree(input) {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+      console.log(i);
+    }
+  }
+}
+
+divisibleByThree();
