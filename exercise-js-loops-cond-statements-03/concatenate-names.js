f5e8059947151da57a3b43bd9c2ea0da85a94228
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-29 10:25:56 +0300

    concatenate names completed

---
+++ exercise-js-loops-cond-statements-03/concatenate-names.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function names(first, second, del) {
+  console.log(`${first}${del}${second} `);
+}
+
+names("John", "Smith", "->");
+names("Jan", "White", "<->");
+names("Linda", "Terry", "=>");
