494807e1101fdeed04fd321a302cc8b7597ef5df
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-29 11:24:26 +0300

    task triples completed

---
+++ exercise-js-loops-cond-statements-03/triples-of-latin.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function triples(num) {
+  let number = Number(num);
+  for (let i = 0; i < number; i++) {
+    let firstLetter = String.fromCharCode(97 + i);
+    for (let j = 0; j < number; j++) {
+      let secondLetter = String.fromCharCode(97 + j);
+      for (let k = 0; k < number; k++) {
+        let thirdLetter = String.fromCharCode(97 + k);
+        console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+      }
+    }
+  }
+}
+
+triples("3");
