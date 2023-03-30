1a1a26a92c8a37bb89b07b5cebac83f8b9f1d2d4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-30 12:02:42 +0300

    gladiator expenses done

--- exercise-js-loops-cond-statements-04/gladiator-expenses.js
+++ exercise-js-loops-cond-statements-04/gladiator-expenses.js
@@ -54,3 +54,31 @@ function gladiatorExpenses(
 
 gladiatorExpenses(7, 2, 3, 4, 5);
 gladiatorExpenses(23, 12.5, 21.5, 40, 200);
+
+// function gladiatorExpenses(
+//   lostFights,
+//   helmetPrice,
+//   swordPrice,
+//   shieldPrice,
+//   armorPrice
+// ) {
+//   let sum = 0;
+//   for (let i = 1; i <= lostFights; i++) {
+//     if (i % 2 ==== 0) {
+//       sum += helmetPrice;
+//     }
+//     if (i % 3 === 0) {
+//       sum += swordPrice;
+//     }
+//     if (i % 6 === 0) {
+//       sum += shieldPrice;
+//     }
+//     if (i % 12 === 0) {
+//       sum += armorPrice;
+//     }
+//   }
+//   console.log(`Gladiator expenses: ${sum.toFixed(2)} aureus`)
+// }
+
+// gladiatorExpenses(7, 2, 3, 4, 5);
+// gladiatorExpenses(23, 12.5, 21.5, 40, 200);
