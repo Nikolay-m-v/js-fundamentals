5f5c974d609ea1a09035c59bede4e38857bd2bdc
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-03-30 10:44:52 +0300

    task town info done

---
+++ exercise-js-loops-cond-statements-04/town-info.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function townInfo(name, population, area) {
+  console.log(
+    `Town ${name} has population of ${population} and area ${area} square km.`
+  );
+}
+
+townInfo("Sofia", 1286383, 492);
+townInfo("Plovdiv", 1481353, 512);
