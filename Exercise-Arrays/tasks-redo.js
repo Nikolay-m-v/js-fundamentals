71ef3a83ec2235575a11c066dadd2850304b80a3
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-06 09:41:46 +0300

    add and remove redone

--- Exercise-Arrays/tasks-redo.js
+++ Exercise-Arrays/tasks-redo.js
@@ -11,3 +11,27 @@ function printNth(input) {
 }
 
 printNth(["5", "20", "31", "4", "20", "2"]);
+
+function addAndRemove(input) {
+  let startNumber = [];
+  let counter = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    counter++;
+    let command = input[i];
+    if (command === "add") {
+      startNumber.push(counter);
+    } else {
+      startNumber.pop();
+    }
+  }
+  if (startNumber.length < 1) {
+    console.log("Empty");
+  } else {
+    console.log(startNumber);
+  }
+}
+
+addAndRemove(["add", "add", "add", "add"]);
+addAndRemove(["add", "add", "remove", "add", "add"]);
+addAndRemove(["remove", "remove", "remove"]);
