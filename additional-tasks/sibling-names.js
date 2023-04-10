b76a031373125632d6dc30614df44aa47dccf46f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-10 10:01:08 +0300

    sibling names done

---
+++ additional-tasks/sibling-names.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function siblingNames() {
+  let arr1 = ["Wesko", "Miro", "Elka"];
+  let arr2 = ["Pamela", "William"];
+  let array = arr1.concat(arr2);
+
+  console.log(array);
+  return array;
+}
+siblingNames();
