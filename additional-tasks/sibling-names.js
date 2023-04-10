61847def75a00ddf90b29fb63037f454fcaf4696
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-10 10:11:36 +0300

    changed one of the arrays names

--- additional-tasks/sibling-names.js
+++ additional-tasks/sibling-names.js
@@ -2,6 +2,7 @@
 
 function siblingNames() {
   let arr1 = ["Wesko", "Miro", "Elka"];
+  arr1[0] = "Brother";
   let arr2 = ["Pamela", "William"];
   let array = arr1.concat(arr2);
   let pet = "Ragnar";
