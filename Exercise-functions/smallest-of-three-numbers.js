a5b0b7c7e95597144a6c6211e2ffa6689038fd73
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-13 11:21:55 +0300

    smallest num done

---
+++ Exercise-functions/smallest-of-three-numbers.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function smallestOfInput(num1, num2, num3) {
+  let smallestNum = Math.min(num1, num2, num3);
+  console.log(smallestNum);
+}
+smallestOfInput(2, 5, 3);
+smallestOfInput(100, 44, 66);
