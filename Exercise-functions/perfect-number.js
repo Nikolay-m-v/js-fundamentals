998326c32ad1c9c4b48efe18d543013b519cb7fa
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-16 19:57:54 +0300

    special number done

---
+++ Exercise-functions/perfect-number.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function perfectOrNot(n) {
+  let perfectSoFar = false;
+  let sumOfPositiveDivisors = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
+      sumOfPositiveDivisors += i;
+    }
+    if (sumOfPositiveDivisors === n) {
+      perfectSoFar = true;
+    } else {
+      perfectSoFar = false;
+    }
+  }
+
+  if (perfectSoFar) {
+    console.log("perfect number!");
+  } else {
+    console.log("not so perfect");
+  }
+}
+
+perfectOrNot(6);
+perfectOrNot(28);
+perfectOrNot(1236498);
