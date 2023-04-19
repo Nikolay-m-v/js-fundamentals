42405cc64edc667e705895bf4317489502a1a938
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-19 13:22:06 +0300

    methods join used

--- Array-methods/array-methods-practice.js
+++ Array-methods/array-methods-practice.js
@@ -6,3 +6,8 @@ console.log(stringified);
 
 let namesArray = ["William", "Kamen", "Georgi"];
 console.log(namesArray.join(" "));
+
+let arrayToConcat = ["John", "Doe", "Roberto"];
+let arrayToAdd = ["Vinchenko", "George"];
+let concatenated = arrayToConcat.concat(arrayToAdd);
+console.log(concatenated);
