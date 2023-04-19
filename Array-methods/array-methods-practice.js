0a03d30e656bebdfb24bb1b3d9c50994cfeb957a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-19 13:25:11 +0300

    methods join used

--- Array-methods/array-methods-practice.js
+++ Array-methods/array-methods-practice.js
@@ -11,3 +11,10 @@ let arrayToConcat = ["John", "Doe", "Roberto"];
 let arrayToAdd = ["Vinchenko", "George"];
 let concatenated = arrayToConcat.concat(arrayToAdd);
 console.log(concatenated);
+
+let namesToSplice = ["William", "Kamen", "Georgi"];
+namesToSplice.splice(1, 1);
+console.log(namesToSplice);
+
+let arrayToSlice = ["William", "Kamen", "Georgi", "Vision"];
+let sliced = arrayToSlice.slice(1, 2);
