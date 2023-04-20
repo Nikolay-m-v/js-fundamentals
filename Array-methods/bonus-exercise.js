14de8ec1141e3ce544bc92fb4c1c18322b076061
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-20 16:05:49 +0300

    bonus exercise done

--- Array-methods/bonus-exercise.js
+++ Array-methods/bonus-exercise.js
@@ -3,7 +3,9 @@
 const row = [10, 20, 30, 40, 50];
 
 const matrix = row.map((item) => {
-  return (item = [item]);
+  const array = [];
+  array.push(item);
+  return array;
 });
 
 console.log(matrix);
