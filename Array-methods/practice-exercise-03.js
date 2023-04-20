623759fe9a0dab2d86d08ac0da5afc16ae2f6c31
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-20 15:54:12 +0300

    practice exercise 3 fix

--- Array-methods/practice-exercise-03.js
+++ Array-methods/practice-exercise-03.js
@@ -4,7 +4,7 @@ const prices = [1.23, 19.99, 85.2, 32.87, 8, 5.2];
 
 const taxedPrices = prices.map((price) => {
   if (price > 10) {
-    return (price -= price * 0.2);
+    return price * 1.2;
   } else {
     return price;
   }
