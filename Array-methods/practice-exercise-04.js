4e7240dfacc1bf0f5d9f339b76dcf4fdf4abb024
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-21 23:51:49 +0300

    additional examples added

--- Array-methods/practice-exercise-04.js
+++ Array-methods/practice-exercise-04.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const items = ["light", "banana", "phone", "book", "mouse"];
+const items2 = ["light", "banana", "scorpion", "book", "mouse"];
 
 const plurals = items.map((item) => {
   if (item === "mouse") {
