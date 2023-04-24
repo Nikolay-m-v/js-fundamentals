3343e748e090fbd265e1dbaad454966ec03b52fb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-24 11:16:14 +0300

    new task done

---
+++ 01. Basic Syntax, Conditional Statements and Loops - More Exercise/distance-between-points.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function distanceBetweenPoints(x1, y1, x2, y2) {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+  let distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  console.log(distance);
+}
+
+distanceBetweenPoints(2, 4, 5, 0);
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985);
