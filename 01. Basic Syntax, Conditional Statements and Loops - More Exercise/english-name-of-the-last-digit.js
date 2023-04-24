df520ebdab5b09ba038fae3e3165f75f31b16aba
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-04-24 10:32:19 +0300

    english name Done

---
+++ 01. Basic Syntax, Conditional Statements and Loops - More Exercise/english-name-of-the-last-digit.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function englishName(number) {
+  let numberToString = number.toString();
+  let lastDigitString = numberToString.slice(-1);
+  let lastDigitNumber = Number(lastDigitString);
+
+  switch (lastDigitNumber) {
+    case 0:
+      console.log("zero");
+      break;
+    case 1:
+      console.log("one");
+      break;
+    case 2:
+      console.log("two");
+      break;
+    case 3:
+      console.log("three");
+      break;
+    case 4:
+      console.log("four");
+      break;
+    case 5:
+      console.log("five");
+      break;
+    case 6:
+      console.log("six");
+      break;
+    case 7:
+      console.log("seven");
+      break;
+    case 8:
+      console.log("eight");
+      break;
+    case 9:
+      console.log("nine");
+      break;
+  }
+}
+
+englishName(512);
