21b86f20050ca9b23ffade593378938b2efd3e0d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-01 12:06:14 +0300

    task done

---
+++ 03. Arrays - Exercise/common-elements.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function commonElements(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array2[j] === array1[i]) {
+        console.log(array2[j]);
+      }
+    }
+  }
+}
+
+commonElements(
+  ["Hey", "hello", 2, 4, "Peter", "e"],
+  ["Petar", 10, "hey", 4, "hello", "2"]
+);
+commonElements(
+  ["S", "o", "f", "t", "U", "n", "i", " "],
+  ["s", "o", "c", "i", "a", "l"]
+);
