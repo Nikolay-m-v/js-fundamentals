f6002ddd84630226e501153672a51602ae2c7eff
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-03 19:31:50 +0300

    task done

--- 05. Arrays Advanced - Exercise/train.js
+++ 05. Arrays Advanced - Exercise/train.js
@@ -1,30 +1,25 @@
 "use strict";
 
 function train(array) {
-  let passengersInWagon = array[0].split(" ").map((number) => Number(number));
-  let passengersInCurretnWagon = Number(passengersInWagon[0]);
-  let maxCapacityPerWagon = Number(array[1]);
+  let passengersInWagon = array.shift().split(" ").map(Number);
+  let maxCapacity = Number(array.shift());
   let newArray = [];
-  console.log(passengersInWagon);
-  console.log(passengersInCurretnWagon);
 
-  for (let i = 0; i < passengersInWagon.length; i++) {
-    let currentWagon = passengersInWagon[0];
-    for (let k = 2; k < array.length; k++) {
-      let commands = array[k];
-      let command = commands[0];
-      let numberOfPassengers = Number(commands[1]);
-      if (command === "add") {
-        newArray.concat(numberOfPassengers);
-      } else {
-        if (currentWagon + numberOfPassengers < 76) {
-          currentWagon += numberOfPassengers;
-        } else {
-          if (currentWagon[i + 1] )
+  for (let command of array) {
+    let currentCommand = command.split(" ");
+
+    if (currentCommand[0] === "Add") {
+      passengersInWagon.push(Number(currentCommand[1]));
+    } else {
+      for (let k = 0; k < passengersInWagon.length; k++) {
+        if (passengersInWagon[k] + Number(currentCommand[0]) <= maxCapacity) {
+          passengersInWagon[k] += Number(currentCommand[0]);
+          break;
         }
       }
     }
   }
+  console.log(passengersInWagon.join(" "));
 }
 
 train(["32 54 21 12 4 0 23", "75", "Add 10", "Add 0", "30", "10", "75"]);
