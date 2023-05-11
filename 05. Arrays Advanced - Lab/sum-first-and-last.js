3c308ce8525bbd0d421a7e546ca5ffce9d8e7f44
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-11 13:18:30 +0300

    tasks

--- 05. Arrays Advanced - Lab/sum-first-and-last.js
+++ 05. Arrays Advanced - Lab/sum-first-and-last.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function sumFirstLast(array) {
+  let first = Number(array[0]);
+  let last = Number(array.slice(-1));
+
+  let sum = first + last;
+  console.log(sum);
+}
+
+sumFirstLast(["20", "30", "40"]);
