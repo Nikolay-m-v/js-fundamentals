d3df75467ea4ebf71e120cc4c70c45445774d970
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-14 12:07:26 +0300

    tasks

---
+++ 05. Arrays Advanced - More Exercises/air-pollution.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function airPollution(matrix, conditions) {}
+
+airPollution(
+  [
+    "5 7 72 14 4",
+    "41 35 37 27 33",
+    "23 16 27 42 12",
+    "2 20 28 39 14",
+    "16 34 31 10 24",
+  ],
+  ["breeze 1", "gale 2", "smog 25"]
+);
