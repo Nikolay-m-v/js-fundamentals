4c2e2fb0cbafbe6e693e65492622d7b62253a3dd
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-15 10:40:15 +0300

    task done

--- 06. Objects and Classes - Lab/stove-provision.js
+++ 06. Objects and Classes - Lab/stove-provision.js
@@ -20,7 +20,6 @@ function stoveProvision(stock, products) {
     console.log(`${property} -> ${output[property]}`);
   }
 }
-console.log(output);
 
 stoveProvision(
   ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
