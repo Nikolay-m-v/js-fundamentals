b6575fcf63dbebfc44522e6d1e68ca400f4817c7
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-15 10:50:19 +0300

    task done

--- 06. Objects and Classes - Lab/towns.js
+++ 06. Objects and Classes - Lab/towns.js
@@ -1,15 +1,25 @@
 "use strict";
 
 function towns(array) {
-  for (let i = 0; i < array.length; i++) {
-    let elements = array[i].split("|");
+  let listOfTowns = {};
 
-    console.log(
-      `town: ${elements[0]} latitude: '${Number(elements[1]).toFixed(
-        2
-      )}', longitude: ${Number(elements[2]).toFixed(2)}`
-    );
+  for (let element of array) {
+    let townInfo = element.split(" | ");
+    listOfTowns.name = townInfo[0];
+    listOfTowns.latitude = Number(townInfo[1]).toFixed(2);
+    listOfTowns.longitude = Number(townInfo[2]).toFixed(2);
+    console.log(listOfTowns);
   }
+
+  // for (let i = 0; i < array.length; i++) {
+  //   let elements = array[i].split("|");
+
+  //   console.log(
+  //     `town: ${elements[0]} latitude: '${Number(elements[1]).toFixed(
+  //       2
+  //     )}', longitude: ${Number(elements[2]).toFixed(2)}`
+  //   );
+  // }
 }
 
 towns(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]);
