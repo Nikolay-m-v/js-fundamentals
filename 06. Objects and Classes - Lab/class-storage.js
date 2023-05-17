e090bdcd1643ad5bea76783564c79763246160bb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-17 14:21:08 +0300

    task done

--- 06. Objects and Classes - Lab/class-storage.js
+++ 06. Objects and Classes - Lab/class-storage.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function classStorage() {
-  class storage {
+  class Storage {
     constructor(capacity) {
       this.capacity = capacity;
       this.storage = [];
@@ -10,12 +10,25 @@ function classStorage() {
 
     addProduct(product) {
       this.capacity -= product.quantity;
+      this.totalCost += product.price * product.quantity;
+      this.storage.push(product);
+    }
+
+    getProducts() {
+      return this.storage.map((product) => JSON.stringify(product)).join("\n");
     }
   }
 
   let productOne = { name: "Cucamber", price: 1.5, quantity: 15 };
   let productTwo = { name: "Tomato", price: 0.9, quantity: 25 };
   let productThree = { name: "Bread", price: 1.1, quantity: 8 };
+  let storage = new Storage(50);
+  storage.addProduct(productOne);
+  storage.addProduct(productTwo);
+  storage.addProduct(productThree);
+  console.log(storage.getProducts());
+  console.log(storage.capacity);
+  console.log(storage.totalCost);
 }
 
 classStorage();
