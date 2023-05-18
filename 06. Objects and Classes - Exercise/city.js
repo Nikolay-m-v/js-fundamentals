e5dbfefb13b67455a70787221fbbb213df246347
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-18 11:37:32 +0300

    task done

---
+++ 06. Objects and Classes - Exercise/city.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function city(city) {
+  let props = Object.keys(city);
+
+  for (let prop of props) {
+    console.log(`${prop} -> ${city[prop]}`);
+  }
+}
+
+city({
+  name: "Sofia",
+  area: 492,
+  population: 1238438,
+  country: "Bulgaria",
+  postCode: "1000",
+});
