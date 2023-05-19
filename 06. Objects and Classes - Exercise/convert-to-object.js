659a2dfb2665932af493bca55123d4fcf3be6cdc
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-19 10:19:34 +0300

    tasks

---
+++ 06. Objects and Classes - Exercise/convert-to-object.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function convertToObject(input) {
+  let object = JSON.parse(input);
+  let keys = Object.keys(object);
+  console.log(object);
+  console.log(keys);
+
+  for (let prop of keys) {
+    console.log(`${prop}: ${object[prop]}`);
+  }
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
