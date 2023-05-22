308810cfe9292c05cef1e8ef20544c7d1751abc1
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-22 11:51:06 +0300

    task done

---
+++ 07. Associative Arrays - Exercise/words-tracker.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function wordTracker(array) {
+  let targetWords = array.shift().split(" ");
+  let targetWordsOneCounter = 0;
+  let targetWordsTwoCounter = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentWord = array[i];
+    if (currentWord === targetWords[0]) {
+      targetWordsOneCounter++;
+    }
+    if (currentWord === targetWords[1]) {
+      targetWordsTwoCounter++;
+    }
+  }
+  console.log(`${targetWords[0]} - ${targetWordsOneCounter}`);
+  console.log(`${targetWords[1]} - ${targetWordsTwoCounter}`);
+}
+
+wordTracker([
+  "this sentence",
+  "In",
+  "this",
+  "sentence",
+  "you",
+  "have",
+  "to",
+  "count",
+  "the",
+  "occurrences",
+  "of",
+  "the",
+  "words",
+  "this",
+  "and",
+  "sentence",
+  "because",
+  "this",
+  "is",
+  "your",
+  "task",
+]);
