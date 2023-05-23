f7cc3452c1d2db59f895aa93f5e214937d3b4c0d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-23 10:49:45 +0300

    task done

--- 07. Associative Arrays - Exercise/party-time.js
+++ 07. Associative Arrays - Exercise/party-time.js
@@ -1,5 +1,37 @@
 "use strict";
 
-function partyTime(array) {}
+function partyTime(array) {
+  let vipGuests = [];
+  let regularGuests = [];
 
-partyTime();
+  while (array[0] !== "PARTY") {
+    let reservation = array.shift();
+    reservation[0].charCodeAt() >= 48 && reservation[0].charCodeAt() <= 57
+      ? vipGuests.push(reservation)
+      : regularGuests.push(reservation);
+  }
+
+  for (let guest of array) {
+    if (vipGuests.indexOf(guest) >= 0) {
+      vipGuests.splice(vipGuests.indexOf(guest), 1);
+    }
+    if (regularGuests.indexOf(guest) >= 0) {
+      regularGuests.splice(regularGuests.indexOf(guest), 1);
+    }
+  }
+  console.log(vipGuests.length + regularGuests.length);
+  vipGuests.forEach((el) => console.log(el));
+  regularGuests.forEach((el) => console.log(el));
+}
+
+partyTime([
+  "7IK9Yo0h",
+  "9NoBUajQ",
+  "Ce8vwPmE",
+  "SVQXQCbc",
+  "tSzE5t0p",
+  "PARTY",
+  "9NoBUajQ",
+  "Ce8vwPmE",
+  "SVQXQCbc",
+]);
