4cd18db573f0783b5576ffcc6957c22bd89c0a8e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-24 09:55:41 +0300

    task done

--- 07. Associative Arrays - Exercise/company-users.js
+++ 07. Associative Arrays - Exercise/company-users.js
@@ -28,3 +28,10 @@ companyUsers([
   "Microsoft -> CC12345",
   "HP -> BB12345",
 ]);
+companyUsers([
+  "SoftUni -> AA12345",
+  "SoftUni -> CC12344",
+  "Lenovo -> XX23456",
+  "SoftUni -> AA12345",
+  "Movement -> DD11111",
+]);
