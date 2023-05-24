60d383589c03bef99c2ac315fa9fd89834779a0a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-24 10:26:10 +0300

    task done

--- 07. Associative Arrays - Exercise/travel-time.js
+++ 07. Associative Arrays - Exercise/travel-time.js
@@ -45,3 +45,11 @@ travelTime([
   "Albania > Tirana > 1000",
   "Bulgaria > Sofia > 200",
 ]);
+travelTime([
+  "Bulgaria > Sofia > 25000",
+  "Bulgaria > Sofia > 25000",
+  "Kalimdor > Orgrimar > 25000",
+  "Albania > Tirana > 25000",
+  "Bulgaria > Varna > 25010",
+  "Bulgaria > Lukovit > 10",
+]);
