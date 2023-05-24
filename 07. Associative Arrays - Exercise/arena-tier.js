dc2fad782719606b6f5f24ba765e80ca501dc8e9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-24 11:24:41 +0300

    task done

--- 07. Associative Arrays - Exercise/arena-tier.js
+++ 07. Associative Arrays - Exercise/arena-tier.js
@@ -68,13 +68,13 @@ function arenaTier(array) {
     });
 }
 
-// arenaTier([
-//   "Peter -> BattleCry -> 400",
-//   "Alex -> PowerPunch -> 300",
-//   "Stefan -> Duck -> 200",
-//   "Stefan -> Tiger -> 250",
-//   "Ave Cesar",
-// ]);
+arenaTier([
+  "Peter -> BattleCry -> 400",
+  "Alex -> PowerPunch -> 300",
+  "Stefan -> Duck -> 200",
+  "Stefan -> Tiger -> 250",
+  "Ave Cesar",
+]);
 arenaTier([
   "Peter -> Duck -> 400",
   "Julius -> Shield -> 150",
