568be9e7a62f7d0efa9832a489953a8f95b71adb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-24 10:04:25 +0300

    task done

---
+++ 07. Associative Arrays - Exercise/miner-task.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function minerTask(array) {
+  let resources = {};
+
+  for (let i = 0; i < array.length; i += 2) {
+    let [resource, quantity] = [array[i], array[i + 1]];
+    !resources.hasOwnProperty(resource)
+      ? (resources[resource] = Number(quantity))
+      : (resources[resource] += Number(quantity));
+  }
+  for (let resource in resources) {
+    console.log(`${resource} -> ${resources[resource]}`);
+  }
+}
+
+minerTask(["Gold", "155", "Silver", "10", "Copper", "17"]);
