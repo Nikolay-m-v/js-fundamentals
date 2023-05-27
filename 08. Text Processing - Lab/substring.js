5621fb7f16a60ff22c5d0ab64f9389bb159a4886
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-27 12:21:51 +0300

    task done

---
+++ 08. Text Processing - Lab/substring.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function substring(string, number1, number2) {
+  console.log(string.substring(number1, number1 + number2));
+}
+
+substring("ASentence", 1, 8);
+substring("SkipWord", 4, 7);
