652d2c41973375bb73fec807160ef0d6829fa817
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-30 12:14:51 +0300

    task done

--- 08. Text Processing - More Exercise/deserialize-string.js
+++ 08. Text Processing - More Exercise/deserialize-string.js
@@ -24,3 +24,14 @@ function deserializeString(array) {
 }
 
 deserializeString(["a:0/2/4/6", "b:1/3/5", "end"]);
+deserializeString([
+  "a:0/3/5/11",
+  "v:1/4",
+  "j:2",
+  "m:6/9/15",
+  "s:7/13",
+  "d:8/14",
+  "c:10",
+  "l:12",
+  "end",
+]);
