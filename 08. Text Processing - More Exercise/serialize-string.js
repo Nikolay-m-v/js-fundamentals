28f1773cf3ded6e09b0bc38220cbdfa90b20d9cc
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-05-30 12:04:01 +0300

    task done

---
+++ 08. Text Processing - More Exercise/serialize-string.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function serializeString(array) {
+  let [word] = array;
+  let letters = {};
+
+  for (let i = 0; i < word.length; i++) {
+    let currLetter = word[i];
+
+    if (!letters.hasOwnProperty(currLetter)) {
+      letters[currLetter] = [i];
+    } else {
+      letters[currLetter].push(i);
+    }
+  }
+
+  for (let letter in letters) {
+    console.log(`${letter}:${letters[letter].join("/")}`);
+  }
+}
+
+serializeString(["abababa"]);
