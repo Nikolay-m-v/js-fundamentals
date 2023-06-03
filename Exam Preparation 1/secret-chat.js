3ba72e335dce9a41ec9191541aff4dba4b23d902
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-03 12:30:13 +0300

    exam tasks

--- Exam Preparation 1/secret-chat.js
+++ Exam Preparation 1/secret-chat.js
@@ -10,6 +10,20 @@ function secretChat(input) {
     if (command === "InsertSpace") {
       let index = line[1];
       let firstHalf = message.substring(0, index);
+      let secondHalf = message.substring(index);
+      message = firstHalf + " " + secondHalf;
+      console.log(message);
+    } else if (command === "Reverse") {
+      let substring = line[1].reverse();
+      if (!message.includes(substring)) {
+        console.log("error");
+      } else {
+        let index = message.indexOf(substring);
+        let firstHalf = message.substring(0, index);
+        let secondHalf = message.substring(index + substring.length);
+        message =
+          firstHalf + secondHalf + substring.split("").reverse().join("");
+      }
     }
   }
 }
