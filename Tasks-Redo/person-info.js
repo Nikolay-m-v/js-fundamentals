b3da16e0ddf597ae0fed1f647ffd63bdf2824709
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-04 11:39:39 +0300

    task done

---
+++ Tasks-Redo/person-info.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function personInfo(firstName, lastName, age) {
+  let person = { firstName: firstName, lastName: lastName, age: age };
+  return person;
+}
+
+console.log(personInfo("Peter", "Pan", "20"));
