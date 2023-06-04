38d5318f4556b1159cdddfb4ff1cd66d23d5a97d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-04 11:25:57 +0300

    task done

---
+++ mirror-words.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function mirrorWords(input) {}
+
+mirrorWords([
+  "@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r",
+]);
