daa12efe563eb4cfe59ea3214b579a45ffe3f0c9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-04 12:18:34 +0300

    task done

--- Tasks-Redo/06. Objects and Classes - Exercise/employees.js
+++ Tasks-Redo/06. Objects and Classes - Exercise/employees.js
@@ -1,4 +1,20 @@
 "use strict";
 
+function employees(input) {
+  let personInfo = {};
 
-function employees();
+  for (let employee of input) {
+    personInfo.name = employee;
+    personInfo.personalNum = employee.length;
+    console.log(
+      `Name: ${personInfo.name} -- Personal Number: ${personInfo.personalNum}`
+    );
+  }
+}
+
+employees([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
