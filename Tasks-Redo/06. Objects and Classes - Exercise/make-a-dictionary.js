b4f4adc9060cd5852b684becfea12073024b1d57
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-05 11:30:47 +0300

    task done

--- Tasks-Redo/06. Objects and Classes - Exercise/make-a-dictionary.js
+++ Tasks-Redo/06. Objects and Classes - Exercise/make-a-dictionary.js
@@ -8,7 +8,7 @@ function makeADicitonary(input) {
     dictionary = Object.assign(dictionary, object);
   }
 
-  let sortedKeys = Object.keys(dicitonary);
+  let sortedKeys = Object.keys(dictionary);
   sortedKeys.sort((a, b) => a.localeCompare(b));
 
   for (let term of sortedKeys) {
@@ -17,7 +17,7 @@ function makeADicitonary(input) {
   }
 }
 
-dicitonary([
+dictionary([
   '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
   '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
   '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
