356a2b07c10ebf2c8e2a764173d13e250d4871b7
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-02 11:33:31 +0300

    exam tasks

--- Exam Preparation 2/imitation-game.js
+++ Exam Preparation 2/imitation-game.js
@@ -15,8 +15,17 @@ function imitationGame(input) {
     } else if (command === "Insert") {
       let index = Number(line[1]);
       let value = line[2];
+      let leftHalf = message.substring(0, index);
+      let rightHalf = message.substring(index);
+      message = leftHalf + value + rightHalf;
+    } else if ((command = "ChangeAll")) {
+      let substring = line[1];
+      let replacement = line[2];
+      message = message.split(substring).join(replacement);
     }
   }
+
+  console.log(`The decrypted message is: ${message}`);
 }
 
 imitationGame(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
