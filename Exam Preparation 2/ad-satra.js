b002f6473b31e8e7f61fb8c65795c1baecc62ea2
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-02 12:17:15 +0300

    exam tasks

---
+++ Exam Preparation 2/ad-satra.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function adSatra(input) {
+  let pattern =
+    /([#|\|])(?<item>[\sA-Za-z]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<nutrition>\d+)\1/g;
+
+  let match = pattern.exec(input);
+  let totalCalories = 0;
+  let output = [];
+
+  while (match !== null) {
+    output.push(
+      `Item: ${match.groups.item}, Best before: ${
+        match.groups.date
+      }, Nutrition: ${Number(match.groups.nutrition)}`
+    );
+    totalCalories += Number(match.groups.nutrition);
+    match = pattern.exec(input);
+  }
+
+  console.log(
+    `You have food to last you for: ${Math.floor(totalCalories / 2000)} days!`
+  );
+}
+
+adSatra(
+  "#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|"
+);
