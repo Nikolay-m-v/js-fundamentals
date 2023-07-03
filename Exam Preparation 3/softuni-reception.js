749c94e72a888d1fcef1daa8ff42f4d279f6337e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-03 09:16:16 +0300

    task done

---
+++ Exam Preparation 3/softuni-reception.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function reception(input) {
+  let studentsCount = Number(input.pop());
+  let employeeOne = Number(input.shift());
+  let employeeTwo = Number(input.shift());
+  let employeeThree = Number(input.shift());
+  let hoursNeeded = 0;
+
+  let studentsPerHour = employeeOne + employeeTwo + employeeThree;
+
+  while (studentsCount > 0) {
+    studentsCount -= studentsPerHour;
+    hoursNeeded++;
+  }
+  console.log(`Hours needed: ${hoursNeeded}`);
+}
+
+reception(["5", "6", "4", "20"]);
