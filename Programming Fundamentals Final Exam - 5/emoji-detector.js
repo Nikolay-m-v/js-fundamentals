0a194505725915576468bd0e8333ec2a3e4056e3
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-22 22:27:56 +0300

    tasks

--- Programming Fundamentals Final Exam - 5/emoji-detector.js
+++ Programming Fundamentals Final Exam - 5/emoji-detector.js
@@ -39,5 +39,5 @@ function emojiDetector(input) {
 }
 
 emojiDetector([
-  "In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**",
+  "In the Sofia Zoo there are at this moment 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**",
 ]);
