e812a3380d68d0016417983fc4ead422d4eff56a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-26 11:07:25 +0300

    extra objects training

--- Tasks-Redo/extra-training-objects/object-training.js
+++ Tasks-Redo/extra-training-objects/object-training.js
@@ -11,3 +11,14 @@ let owner = {};
 owner.name = "Nikolay";
 owner.age = 29;
 owner.isMale = true;
+
+function objectTraining(firstName, lastName, age) {
+  let result = {
+    name: firstName,
+    lastName: lastName,
+    age: age,
+  };
+  console.log(result);
+}
+
+objectTraining("Ivan", "Ivanov", 20);
