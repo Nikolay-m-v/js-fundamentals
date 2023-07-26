009a080b0edf84e42db807e903fbedef1e3b1a28
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-26 11:33:56 +0300

    extra objects training

--- Tasks-Redo/extra-training-objects/iteration-objects.js
+++ Tasks-Redo/extra-training-objects/iteration-objects.js
@@ -6,14 +6,20 @@ let cat = {
   breed: "TurkishAngorish",
 };
 
-for (const prop in cat) {
-  console.log(cat[prop]);
-}
+// for (const prop in cat) {
+//   console.log(cat[prop]);
+// }
 
 let propName = "age";
 
 // console.log(cat[propName]);
 // console.log(Object.values(cat));
+// console.log(Object.keys(cat));
+// console.log(Object.entries(cat));
 
 let arrayCat = Object.values(cat);
-console.log(arrayCat);
+// console.log(arrayCat);
+
+for (const key of Object.keys(cat)) {
+  console.log(cat[key]);
+}
