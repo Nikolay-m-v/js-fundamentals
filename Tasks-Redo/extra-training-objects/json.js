5bfd86ff9329437c2b7e102d410b2d94066a7eec
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-28 11:30:44 +0300

    task

---
+++ Tasks-Redo/extra-training-objects/json.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let jsonData = {
+//   "name": "Maria",
+//   "age": 20,
+//   "grades": {
+//     "Math": [6.00, 5.50],
+//     "Biology": [6.00]
+//   }
+// }
+
+let jsData = {
+  name: "Maria",
+  age: 20,
+  grades: {
+    Math: [6.0, 5.5],
+    Biology: [6.0],
+  },
+};
