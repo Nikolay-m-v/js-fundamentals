d114935459dc78d4ccbd872b2ff3ad27bb841884
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-28 11:48:07 +0300

    task

---
+++ Tasks-Redo/extra-training-objects/convert-to-json.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function convertor(firstName, lastName, color) {
+  let obj = {
+    name: firstName,
+    lastName: lastName,
+    eyeColor: color,
+  };
+
+  let objToJson = JSON.stringify(obj);
+  console.log(objToJson);
+}
+
+convertor("Ivan", "Ivanov", "blue");
