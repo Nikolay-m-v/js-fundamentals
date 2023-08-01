8246eceeca4b1d07ec3185d1c2df65d2efd99b43
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-08-01 11:21:41 +0300

    task done

--- Tasks-Redo/extra-training-objects/bank.js
+++ Tasks-Redo/extra-training-objects/bank.js
@@ -3,7 +3,10 @@
 let bankInfo = {
   owner: "Ivan",
   amount: 1000,
+  age: 33,
   hasAcces: true,
+  annualInterest: 0.006,
+  yearsOfInterest: 0,
 
   suspend() {
     this.hasAcces = false;
@@ -16,9 +19,26 @@ let bankInfo = {
   withdraw(money) {
     this.amount -= money;
   },
+
+  becomeOlder() {
+    this.age += 1;
+  },
+
+  interest() {
+    if (this.age > 33) {
+      this.yearsOfInterest = this.age - 33;
+      for (let i = 1; i <= this.yearsOfInterest; i++) {
+        this.amount += this.amount * this.annualInterest;
+      }
+    }
+  },
 };
 
+bankInfo.becomeOlder();
 bankInfo.deposit(500);
 bankInfo.withdraw(800);
 bankInfo.suspend();
+bankInfo.interest();
+console.log(bankInfo.age);
 console.log(bankInfo.hasAcces);
+console.log(bankInfo.amount);
